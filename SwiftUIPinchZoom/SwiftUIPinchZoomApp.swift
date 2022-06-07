0e4f31121d3a28fa8ef5ea3984cdc7d09f453cb6
erainer <emily.rainer@lextech.com>
2022-06-07 12:19:35 -0500

    Initial Commit

---
+++ SwiftUIPinchZoom/SwiftUIPinchZoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPinchZoomApp.swift
+//  SwiftUIPinchZoom
+//
+//  Created by Emily Rainer on 6/7/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPinchZoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
